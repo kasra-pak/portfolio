b3b61b1dcf473ff7a2124714559b9717137440f5
kasra paketeghad <kasrapak69@gmail.com>
2022-08-25 07:58:32 +0430

    little late first init

---
+++ script.js
@@ -0,0 +1,31 @@
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const logoEl = document.querySelector(".logo");
+const nameEl = document.querySelector(".name");
+const navLinksEl = document.querySelector(".nav-links");
+
+hamburgerBtn.addEventListener("click", toggleMobileMenu);
+nameEl.addEventListener("transitionend", showMobileMenu);
+
+function toggleMobileMenu(e) {
+  const pressed = e.currentTarget.getAttribute("aria-pressed");
+
+  if (pressed === "true") {
+    e.currentTarget.setAttribute("aria-pressed", "false");
+    navLinksEl.classList.remove("open");
+  } else {
+    e.currentTarget.setAttribute("aria-pressed", "true");
+  }
+
+  nameEl.classList.toggle("hidden");
+}
+
+function showMobileMenu(e) {
+  const nameIsHidden =
+    e.propertyName === "opacity" &&
+    e.currentTarget.classList.contains("hidden");
+
+  if (nameIsHidden) {
+    e.currentTarget.classList.add("removed");
+    navLinksEl.classList.add("open");
+  }
+}
