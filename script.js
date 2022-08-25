de07752150be74713ee9e6d5ab31e984eed720e5
kasra paketeghad <kasrapak69@gmail.com>
2022-08-25 15:52:49 +0430

    Main & Footer added

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-const hamburgerBtn = document.querySelector(".hamburger-btn");
-const logoEl = document.querySelector(".logo");
-const nameEl = document.querySelector(".name");
-const navLinksEl = document.querySelector(".nav-links");
+// // // // // // // // these linse refer to animating mobile menu toggle // // // // // // // //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// const hamburgerBtn = document.querySelector(".hamburger-btn");
+// const logoEl = document.querySelector(".logo");
+// const nameEl = document.querySelector(".name");
+// const navLinksEl = document.querySelector(".nav-links");
 
-hamburgerBtn.addEventListener("click", toggleMobileMenu);
-nameEl.addEventListener("transitionend", showMobileMenu);
+// hamburgerBtn.addEventListener("click", toggleMobileMenu);
+// nameEl.addEventListener("transitionend", showMobileMenu);
 
-function toggleMobileMenu(e) {
-  const pressed = e.currentTarget.getAttribute("aria-pressed");
+// function toggleMobileMenu(e) {
+//   const pressed = e.currentTarget.getAttribute("aria-pressed");
 
-  if (pressed === "true") {
-    e.currentTarget.setAttribute("aria-pressed", "false");
-    navLinksEl.classList.remove("open");
-  } else {
-    e.currentTarget.setAttribute("aria-pressed", "true");
-  }
+//   if (pressed === "true") {
+//     e.currentTarget.setAttribute("aria-pressed", "false");
+//     navLinksEl.classList.remove("open");
+//   } else {
+//     e.currentTarget.setAttribute("aria-pressed", "true");
+//   }
 
-  nameEl.classList.toggle("hidden");
-}
+//   nameEl.classList.toggle("hidden");
+// }
 
-function showMobileMenu(e) {
-  const nameIsHidden =
-    e.propertyName === "opacity" &&
-    e.currentTarget.classList.contains("hidden");
+// function showMobileMenu(e) {
+//   const nameIsHidden =
+//     e.propertyName === "opacity" &&
+//     e.currentTarget.classList.contains("hidden");
 
-  if (nameIsHidden) {
-    e.currentTarget.classList.add("removed");
-    navLinksEl.classList.add("open");
-  }
-}
+//   if (nameIsHidden) {
+//     e.currentTarget.classList.add("removed");
+//     navLinksEl.classList.add("open");
+//   }
+// }
