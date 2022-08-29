1d0e61eee2ef9d09e7e876471155b451630d6bc3
kasra paketeghad <kasrapak69@gmail.com>
2022-08-29 17:37:44 +0430

    Added fading scroll on the navbar: very small screens

--- script.js
+++ script.js
@@ -32,3 +32,33 @@
 //     navLinksEl.classList.add("open");
 //   }
 // }
+
+const navLinks = document.querySelector(".nav-links");
+const fadeScroll = document.querySelector(".fade-scroll");
+
+// console.log(navLinks);
+
+window.addEventListener("load", toggleFadeScrollEffect);
+window.addEventListener("resize", toggleFadeScrollEffect);
+navLinks.addEventListener("scroll", toggleFadeScrollEffect);
+
+function toggleFadeScrollEffect() {
+  const overflow = navLinks.clientWidth < navLinks.scrollWidth;
+  const isScrollAtStart = navLinks.scrollLeft === 0;
+  const isScrollAtEnd = navLinks.scrollLeft === navLinks.scrollLeftMax;
+
+  if (overflow) {
+    if (isScrollAtStart) {
+      fadeScroll.classList.add("fade-right");
+      fadeScroll.classList.remove("fade-left");
+    } else if (isScrollAtEnd) {
+      fadeScroll.classList.add("fade-left");
+      fadeScroll.classList.remove("fade-right");
+    } else {
+      fadeScroll.classList.add("fade-left");
+      fadeScroll.classList.add("fade-right");
+    }
+  } else {
+    fadeScroll.classList.remove("fade-left", "fade-right");
+  }
+}
