fc2dd7e693b40a7da7377f86dd0d8a403e0f4837
kasra paketeghad <kasrapak69@gmail.com>
2022-08-30 11:42:32 +0430

    Responsiveness: Large Screens

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
-// // // // // // // // these linse refer to animating mobile menu toggle // // // // // // // //
-// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
-// const hamburgerBtn = document.querySelector(".hamburger-btn");
-// const logoEl = document.querySelector(".logo");
-// const nameEl = document.querySelector(".name");
-// const navLinksEl = document.querySelector(".nav-links");
-
-// hamburgerBtn.addEventListener("click", toggleMobileMenu);
-// nameEl.addEventListener("transitionend", showMobileMenu);
-
-// function toggleMobileMenu(e) {
-//   const pressed = e.currentTarget.getAttribute("aria-pressed");
-
-//   if (pressed === "true") {
-//     e.currentTarget.setAttribute("aria-pressed", "false");
-//     navLinksEl.classList.remove("open");
-//   } else {
-//     e.currentTarget.setAttribute("aria-pressed", "true");
-//     navLinksEl.classList.add("open");
-//   }
-
-//   nameEl.classList.toggle("hidden");
-// }
-
-// function showMobileMenu(e) {
-//   const nameIsHidden =
-//     e.propertyName === "opacity" &&
-//     e.currentTarget.classList.contains("hidden");
-
-//   if (nameIsHidden) {
-//     e.currentTarget.classList.add("removed");
-//     navLinksEl.classList.add("open");
-//   }
-// }
-
 const navLinks = document.querySelector(".nav-links");
 const fadeScroll = document.querySelector(".fade-scroll");
-
-// console.log(navLinks);
 
 window.addEventListener("load", toggleFadeScrollEffect);
 window.addEventListener("resize", toggleFadeScrollEffect);
